16d7e8aae4736376221db53362d3c1c0b2c1d9eb
ianlauyin <ianlauyin@gmail.com>
2024-03-29 02:02:22 +0800

    Updated Post and comments date seed

--- db/seeders/20240323144137-post.js
+++ db/seeders/20240323144137-post.js
@@ -18,32 +18,32 @@ module.exports = {
         content:
           "Welcome to our forum! This is a place to introduce yourself and get to know other members of the community.",
         author_id: 1,
-        created_at: new Date(),
-        updated_at: new Date(),
+        created_at: new Date("2024-01-01T03:24:00"),
+        updated_at: new Date("2024-01-01T03:24:00"),
       },
       {
         title: "Favorite Books",
         content:
           "Share your favorite books and recommendations with fellow book lovers. What are you currently reading?",
         author_id: 2,
-        created_at: new Date(),
-        updated_at: new Date(),
+        created_at: new Date("2024-01-02T04:24:00"),
+        updated_at: new Date("2024-01-02T04:24:00"),
       },
       {
         title: 'Book Recommendation: "The Great Gatsby"',
         content:
           "Let's discuss Harper Lee's \"To Kill a Mockingbird\". Share your thoughts on the characters, themes, and moral lessons depicted in the novel.",
         author_id: 1,
-        created_at: new Date(),
-        updated_at: new Date(),
+        created_at: new Date("2024-01-05T03:21:00"),
+        updated_at: new Date("2024-01-05T03:21:00"),
       },
       {
         title: 'Discussion: "To Kill a Mockingbird"',
         content:
           "Let's discuss Harper Lee's \"To Kill a Mockingbird\". Share your thoughts on the characters, themes, and moral lessons depicted in the novel.",
         author_id: 1,
-        created_at: new Date(),
-        updated_at: new Date(),
+        created_at: new Date("2024-01-06T01:12:00"),
+        updated_at: new Date("2024-01-06T01:12:00"),
       },
     ]);
   },
--- db/seeders/20240323144550-comment.js
+++ db/seeders/20240323144550-comment.js
@@ -9,23 +9,23 @@ module.exports = {
         commenter_id: 2,
         content:
           'I read "The Great Gatsby" last year, and I absolutely loved it. The way Fitzgerald captures the essence of the Jazz Age is remarkable.',
-        created_at: new Date(),
-        updated_at: new Date(),
+        created_at: new Date("2024-01-07T03:24:00"),
+        updated_at: new Date("2024-01-07T03:24:00"),
       },
       {
         commented_post_id: 1,
         commenter_id: 2,
         content: "Hello World!",
-        created_at: new Date(),
-        updated_at: new Date(),
+        created_at: new Date("2024-01-08T03:24:00"),
+        updated_at: new Date("2024-01-08T03:24:00"),
       },
       {
         commented_post_id: 3,
         commenter_id: 1,
         content:
           'I completely agree! "The Great Gatsby" is a beautifully written novel with captivating characters and a thought-provoking storyline.',
-        created_at: new Date(),
-        updated_at: new Date(),
+        created_at: new Date("2024-02-10T09:00:00"),
+        updated_at: new Date("2024-02-10T09:00:00"),
       },
     ]);
   },
