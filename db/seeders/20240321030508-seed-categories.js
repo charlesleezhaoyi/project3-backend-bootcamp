c8330302c5afd1ac62539dfcdb3e1b1d97b8fa9a
ianlauyin <ianlauyin@gmail.com>
2024-03-31 21:38:33 +0800

    Fixed seeder bug

--- db/seeders/20240321030508-seed-categories.js
+++ db/seeders/20240321030508-seed-categories.js
@@ -5,29 +5,44 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert("categories", [
       {
+        name: "Fiction",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: "Non-Fiction",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: "Science Fiction",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      { name: "Fantasy", created_at: new Date(), updated_at: new Date() },
+      { name: "Mystery", created_at: new Date(), updated_at: new Date() },
+      {
+        name: "Romance",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
         name: "Horror",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
-        name: "Sci-Fi",
+        name: "Biography",
         created_at: new Date(),
         updated_at: new Date(),
       },
-      {
-        name: "Thriller",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      { name: "Romance", created_at: new Date(), updated_at: new Date() },
-      { name: "Adventure", created_at: new Date(), updated_at: new Date() },
       {
         name: "History",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
-        name: "Biography",
+        name: "Self-Help",
         created_at: new Date(),
         updated_at: new Date(),
       },
