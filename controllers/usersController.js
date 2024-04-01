0fb03069c7669fbb8b3988062231f654186f2e46
qiannnly <qianlinggg@gmail.com>
2024-04-01 17:30:24 +0800

    added logic to create and update users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,36 +1,3 @@
-// const BaseController = require("./baseController");
-
-// class UsersController extends BaseController {
-//   constructor(model) {
-//     super(model);
-//   }
-
-//   async insertUser(req, res) {
-//     const { userEmail, firstName, lastName, phone } = req.body;
-
-//     try {
-//       const newUser = await this.model.findOrCreate({
-//         where: {
-//           email: userEmail,
-//         },
-//         first_name: firstName,
-//         last_name: lastName,
-//         phone: phone,
-//       });
-
-//       return res.json(newUser).json({
-//         message: "User created!",
-//       });
-//     } catch (err) {
-//       return res.status(400).json({
-//         error: true,
-//         msg: err,
-//       });
-//     }
-//   }
-// }
-// module.exports = UsersController;
-
 const BaseController = require("./baseController");
 
 class UsersController extends BaseController {
@@ -39,17 +6,45 @@ class UsersController extends BaseController {
   }
 
   async insertUser(req, res) {
-    const { userEmail, firstName, lastName } = req.body;
+    const { email, firstName, lastName, phone } = req.body;
 
     try {
-      await this.model.findOrCreate({
-        where: {
-          email: userEmail,
+      const user = await this.model.findOrCreate({
+        where: { email: email },
+        defaults: {
+          firstName: firstName,
+          lastName: lastName,
+          phone: phone,
         },
-        first_name: firstName,
-        last_name: lastName,
       });
-      return res.json("succesful!");
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({
+        error: true,
+        msg: err,
+      });
+    }
+  }
+
+  async updateUser(req, res) {
+    const { email, firstName, lastName, phone } = req.body;
+
+    try {
+      const updatedUser = await this.model.update(
+        {
+          email: email,
+          firstName: firstName,
+          lastName: lastName,
+          phone: phone,
+        },
+        {
+          where: {
+            email: email,
+          },
+        }
+      );
+
+      return res.json(updatedUser);
     } catch (err) {
       return res.status(400).json({
         error: true,
